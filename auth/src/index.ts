6393af3d686a10ebdbfb65d403eeb57afde96595
Kamonwat Sookkara <kamonwat@terabytenet.com>
2021-05-26 17:35:32 +0700

    kai444

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -23,7 +23,7 @@ const start = async () => {
   }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!13x3!!!!!!!');
+    console.log('Listening on port 3000!!!!13xx4443!!!!!!!');
   });
 };
 
